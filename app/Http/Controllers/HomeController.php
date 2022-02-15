499a6b257a61c8948f8bd939d6b06556aa14032d
Ilia <ilia.ilyukha@gmail.com>
2022-02-15 15:53:18 +0200

    View products on homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home.index');
+        $products = Product::join('product_description as pd', 'product.id', '=', 'pd.product_id')
+        ->limit(8)
+        ->get();
+
+        return view('home.index', [
+            'products' => $products
+        ]);
     }
 } 
